0ea1e33a3843e1bdb6cf8b32774ac6019c4ca525
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-01-29 14:27:39 +0000

    adding complete and delete functionality for tasks

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,37 @@
 import "./style.scss";
 
 const list = document.querySelector<HTMLUListElement>(".list");
-const form = document.querySelector<HTMLFormElement>("#new-task-form");
-const input = document.querySelector<HTMLInputElement>("");
+const input = document.querySelector<HTMLInputElement>(".new-task__input");
+const submitBtn = document.querySelector<HTMLButtonElement>(".new-task__btn");
 
-if (!list || !form || !input) {
-    throw new Error("Can't find either list, form or input element");
+if (!list || !input || !submitBtn) {
+    throw new Error("Can't find either list, input and btn element");
 }
 
-// form?addEventListener('submit', e =>{
-//   // doesn't refresh the page
-//   e.preventDefault()
-//   if (input?.value == '' || input?.value == null) return
+const addingTask = () => {
+    if (input.value === "") {
+        alert("You cannot add an empty task");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        li.classList.add("list__task");
+        list.appendChild(li);
+        input.value = "";
+        let crossIcon = document.createElement("span");
+        crossIcon.innerHTML = "\u00d7";
+        crossIcon.classList.add("list__task--cross");
+        li.appendChild(crossIcon);
+    }
+};
 
-//   const task = {
+submitBtn.addEventListener("click", addingTask);
 
-//   }
+const handleTaskClick = (e: any) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+};
 
-//   input.value
-// })
+list.addEventListener("click", handleTaskClick, false);
